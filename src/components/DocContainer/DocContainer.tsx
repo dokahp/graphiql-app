fcb76b8b27167c10fdcd881b05fbe56317249e4b
ErmakovEv <ej-88@ya.ru>
2023-05-06 01:06:09 +0300

    feat:+mui's accordion for documentation

--- src/components/DocContainer/DocContainer.tsx
+++ src/components/DocContainer/DocContainer.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { dataAPI } from '../../store/services/APIservice';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const DocumentationContainer = () => {
   const { data: data } = dataAPI.useFetchAllDataQuery();
-  console.log(data);
-  return <div></div>;
+  console.log(data?.data.__schema);
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          Query: <span>{data?.data.__schema.queryType.name}</span>
+        </AccordionSummary>
+        <AccordionDetails>
+          {data?.data.__schema.types[10].fields?.map((field) => (
+            <Accordion key={field.name}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                {field.name} : TYPES
+              </AccordionSummary>
+              <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                  Argument: {field.args ? field.args[0].name : ''}
+                </AccordionSummary>
+              </Accordion>
+            </Accordion>
+          ))}
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
 };
 
 export default DocumentationContainer;
--- src/store/services/schemaType.ts
+++ src/store/services/schemaType.ts
@@ -1,5 +1,5 @@
 export default interface IDataAPI {
-  data: ISchema;
+  data: { __schema: ISchema };
 }
 
 interface ISchema {
