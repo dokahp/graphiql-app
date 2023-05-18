396d019be694ebf2eb42c7035512e855e97aadb1
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-05-19 00:32:30 +0300

    feat: add hotkeys functionality

--- src/components/Request/Request.tsx
+++ src/components/Request/Request.tsx
@@ -16,6 +16,7 @@ import {
   Typography,
 } from '@mui/material';
 import { format } from 'graphql-formatter';
+import { useHotkeys } from 'react-hotkeys-hook';
 import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
 import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded';
 import CleaningServicesRoundedIcon from '@mui/icons-material/CleaningServicesRounded';
@@ -77,6 +78,18 @@ function Request({
       theme: 'colored',
     });
   };
+
+  useHotkeys('ctrl+shift+enter', () => execQuery(), {
+    enableOnContentEditable: true,
+  });
+
+  useHotkeys('ctrl+shift+p', () => handlePrettifyQuery(), {
+    enableOnContentEditable: true,
+  });
+
+  useHotkeys('ctrl+shift+c', () => handleCopyQuery(), {
+    enableOnContentEditable: true,
+  });
 
   return (
     <section
