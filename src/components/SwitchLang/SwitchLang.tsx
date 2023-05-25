8e0cbe6ebafe9ffbb469d5505f52ebb51f3bd02d
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-05-25 20:27:00 +0300

    fix: delete localstorage to save language detection

--- src/components/SwitchLang/SwitchLang.tsx
+++ src/components/SwitchLang/SwitchLang.tsx
@@ -3,69 +3,30 @@ import { styled } from '@mui/material/styles';
 import Switch from '@mui/material/Switch';
 import { useTranslation } from 'react-i18next';
 
-const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-  width: 62,
-  height: 34,
-  padding: 7,
-  '& .MuiSwitch-switchBase': {
-    margin: 1,
-    padding: 0,
-    transform: 'translateX(6px)',
-    '&.Mui-checked': {
-      color: '#fff',
-      transform: 'translateX(22px)',
-      '& .MuiSwitch-thumb:before': {
-        content: '"UK"',
-      },
-      '& + .MuiSwitch-track': {
-        opacity: 1,
-        backgroundColor:
-          theme.palette.mode === 'dark'
-            ? '#8796A5'
-            : theme.palette.primary.light,
-      },
-    },
-  },
-  '& .MuiSwitch-thumb': {
-    backgroundColor:
-      theme.palette.mode === 'dark' ? '#003892' : theme.palette.primary.dark,
-    width: 32,
-    height: 32,
-    '&:before': {
-      content: '"EN"',
-      position: 'absolute',
-      right: '50%',
-      top: '-10%',
-      translate: '50% 50%',
-    },
-  },
-  '& .MuiSwitch-track': {
-    opacity: 1,
-    backgroundColor:
-      theme.palette.mode === 'dark' ? '#8796A5' : theme.palette.primary.light,
-    borderRadius: 20 / 2,
-  },
-}));
-
 export default function SwitchLang() {
-  const [lang, setLang] = useState('en');
+  // const [lang, setLang] = useState('en');
   const { i18n } = useTranslation();
 
   const changeLanguageHandler = (language: string) => {
-    setLang(language);
+    // setLang(language);
     i18n.changeLanguage(language);
   };
 
   const toggleLang = () => {
-    const newLang = lang === 'en' ? 'uk' : 'en';
-    changeLanguageHandler(newLang);
+    // const newLang = lang === 'en' ? 'uk' : 'en';
+    // changeLanguageHandler(newLang);
+  };
+
+  const newSwitchLang = () => {
+    i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
   };
 
   return (
-    <MaterialUISwitch
-      sx={{ m: 1, alignSelf: 'center' }}
-      defaultChecked
-      onClick={toggleLang}
-    />
+    <div>
+      CURRENT LANG: {i18n.language}{' '}
+      <button type="button" onClick={newSwitchLang}>
+        SWITCH TO {i18n.language === 'en' ? 'RU' : 'EN'}
+      </button>
+    </div>
   );
 }
